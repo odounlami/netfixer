326030c613f713dbca3cd933b976e0bea64c124d
Odounlami oscar <odounlamioscar2002@gmail.com>
2026-04-09 02:56:25 +0100

    fix network export link

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { NetworkMonitor } from "./NetworkMonitor.js";
-export { RequestScheduler } from "./RequestScheduler.js";
+export type { NetworkState, NetworkInfo, NetworkMonitorOptions, NetworkThresholds } from "./NetworkMonitor.js";
 
+export { RequestScheduler } from "./RequestScheduler.js";
 export type { RequestPriority, PriorityRules, SchedulerRequestInit, RequestSchedulerOptions } from "./RequestScheduler.js";
